f0b5c7909ff54a6250f24e3936475d0e0f2c2b87
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-10 18:19:38 -0400

    Added Dashboard test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {render, cleanup, findAllByTestId} from 'react-testing-library';
+import 'jest-dom/extend-expect'
+
+import Dashboard from './Dashboard'
+
+describe('<Dashboard/>', () => {
+    test('should match snapshot', () => {
+        const tree = renderer.create(<Dashboard/>).toJSON()
+
+        expect(tree).toMatchSnapshot()
+    })
+})
